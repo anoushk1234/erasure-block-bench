e661f2d8a52751e16ec6fc28a9aa25a60edf5d13
anoushk1234 <kharangateanoushk04@gmail.com>
2023-05-26 11:20:14 +0000

    something

--- benches/erasure_code_block.rs
+++ benches/erasure_code_block.rs
@@ -1,21 +1,45 @@
+use rand::Rng;
+
 use {erasure_block_bench::*, glassbench::*};
 
 fn erasure_block_bench_gf8(b: &mut Bench) {
-    let (mc, r) = create_block_data_shards(256);
+    let (mc, r) = create_block_data_shards(256, 20000);
     b.task(
-        "Encoding - block size: 2.56MB, shard size: 20KB, n:128, k:128",
+        "Encoding gf8 - block size: 2.56MB, shard size: 20KB, n:128, k:128",
         |task| {
             task.iter(|| encode_block_gf8(mc.clone(), r.clone()));
         },
     );
 
-    let recovery_shards = simulate_loss(mc, 128);
+    let recovery_shards = simulate_loss_gf8(mc, 128);
     b.task(
-        "Recovering - block size: 2.56MB, shard size: 20KB, n:128, k:128",
+        "Recovering gf8 - block size: 2.56MB, shard size: 20KB, n:128, k:128",
         |task| {
             task.iter(|| recover_block_gf8(recovery_shards.clone(), r.clone()));
         },
     );
+    let mut original = vec![];
+    for _ in 0..2000 {
+        let mut elem: [u8; 1280] = [0u8; 1280];
+        for i in 0..1280 {
+            elem[i] = rand::thread_rng().gen::<u8>()
+        }
+        original.push(elem.clone());
+    }
+
+    b.task(
+        "Encoding gf16 - block size: 2.56MB, shard size: 1280 bytes, n:2000, k:2000",
+        |task| {
+            task.iter(|| encode_block_gf16(original.clone(), 2000, 2000));
+        },
+    );
+    // let (dr, er) = simulate_loss_gf16(original);
+    // b.task(
+    //     "Recovering gf16 - block size: 2.56MB, shard size: 1280 bytes, n:2000, k:2000",
+    //     |task| {
+    //         task.iter(|| recover_block_gf16(2000, 2000, dr.clone(), er.clone()));
+    //     },
+    // );
 }
 
 glassbench!(
--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,40 @@
 //! erasure code an entire block instead of just batches
 //! A shard here is 1280 bytes
 #![feature(test)]
+// pub mod macros;
+
 extern crate solana_reed_solomon_erasure;
 
 use rand::{self, Rng};
 use solana_reed_solomon_erasure::*;
-use std::time::Instant;
+// use std::time::Instant;
 
-pub fn create_block_data_shards(data_shards: u32) -> (Vec<Box<[u8]>>, ReedSolomon) {
+pub fn create_block_data_shards(
+    data_shards: u32,
+    shard_size: u32,
+) -> (Vec<Box<[u8]>>, ReedSolomon) {
     let r = ReedSolomon::new(128, 128).unwrap();
 
     // Construct the parity shards
     let mut original = vec![];
     for _ in 0..data_shards {
-        let mut elem: [u8; 20000] = [0u8; 20000];
-        for i in 0..20000 {
-            elem[i] = rand::thread_rng().gen::<u8>()
+        let mut elem: Vec<u8> = Vec::with_capacity(shard_size as usize);
+        for _ in 0..shard_size {
+            elem.push(0);
+        }
+        let mut elem = elem.into_boxed_slice();
+        for i in 0..shard_size {
+            elem[i as usize] = rand::thread_rng().gen::<u8>()
         }
         original.push(elem.clone());
     }
-    let mut boxed_master_copy: Vec<Box<[u8]>> = Vec::default();
-    for i in 0..(original.len()) {
-        boxed_master_copy.push(Box::new(original[i]));
-    }
-    return (boxed_master_copy, r);
+
+    return (original, r);
 }
-pub fn simulate_loss(boxed_master_copy: Vec<Box<[u8]>>, range_end: u32) -> Vec<Option<Box<[u8]>>> {
+pub fn simulate_loss_gf8(
+    boxed_master_copy: Vec<Box<[u8]>>,
+    range_end: u32,
+) -> Vec<Option<Box<[u8]>>> {
     let mut recon_shards = shards_into_option_shards(boxed_master_copy.to_vec());
     for _ in 0..range_end {
         let index = rand::thread_rng().gen_range(0..range_end);
@@ -41,3 +50,43 @@ pub fn encode_block_gf8(mut boxed_master_copy: Vec<Box<[u8]>>, r: ReedSolomon) {
 pub fn recover_block_gf8(mut recon_shards: Vec<Option<Box<[u8]>>>, r: ReedSolomon) {
     r.reconstruct_shards(&mut recon_shards).unwrap();
 }
+
+pub fn encode_block_gf16(original: Vec<[u8; 1280]>, n: usize, k: usize) {
+    reed_solomon_16::encode(
+        n,                // total number of original shards
+        k,                // total number of recovery shards
+        original.clone(), // all original shards
+    )
+    .unwrap();
+}
+// pub fn recover_block_gf16(
+//     n: usize,
+//     k: usize,
+//     data_shard_tuple: Vec<(usize, [u8; 1280])>,
+//     parity_shard_tuple: Vec<(usize, Vec<u8>)>,
+// ) {
+//     let _restored = reed_solomon_16::decode(
+//         n, // total number of original shards
+//         k, // total number of recovery shards
+//         data_shard_tuple[0..1000].to_owned(),
+//         parity_shard_tuple[0..1000].to_owned(),
+//     )
+//     .unwrap();
+// }
+
+// pub fn simulate_loss_gf16(
+//     original: Vec<[u8; 1280]>,
+//     erasure_shards: Vec<Vec<u8>>,
+// ) -> (Vec<(usize, [u8; 1280])>, Vec<(usize, Vec<u8>)>) {
+//     let data_shard_tuple = original
+//         .clone()
+//         .into_iter()
+//         .enumerate()
+//         .collect::<Vec<(usize, [u8; 1280])>>();
+//     let parity_shard_tuple = erasure_shards
+//         .clone()
+//         .into_iter()
+//         .enumerate()
+//         .collect::<Vec<(usize, Vec<u8>)>>();
+//     return (data_shard_tuple, parity_shard_tuple);
+// }
